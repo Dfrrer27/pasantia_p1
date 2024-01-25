1674b4bb4479135ad470a4290a753847cf6e5539
Dfrrer27 <diego.ferrer@tecsup.edu.pe>
2024-01-24 20:54:28 -0500

    cambios de colores

---
+++ js/main.js
@@ -0,0 +1,87 @@
+
+(function ($) {
+    "use strict";
+    
+    $(document).ready(function () {
+        // Manejar clics en enlaces del navbar
+        $(".nav-item.nav-link").on("click", function (event) {
+            // Prevenir el comportamiento predeterminado del enlace
+            event.preventDefault();
+
+            // Obtener el destino del enlace (id de la sección)
+            var targetId = $(this).attr("href");
+
+            // Desplazamiento suave al destino
+            $("html, body").animate({
+                scrollTop: $(targetId).offset().top
+            }, 800); // Ajusta la duración según tus preferencias
+        });
+    });
+
+    // Dropdown on mouse hover
+    $(document).ready(function () {
+        function toggleNavbarMethod() {
+            if ($(window).width() > 992) {
+                $('.navbar .dropdown').on('mouseover', function () {
+                    $('.dropdown-toggle', this).trigger('click');
+                }).on('mouseout', function () {
+                    $('.dropdown-toggle', this).trigger('click').blur();
+                });
+            } else {
+                $('.navbar .dropdown').off('mouseover').off('mouseout');
+            }
+        }
+        toggleNavbarMethod();
+        $(window).resize(toggleNavbarMethod);
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+    
+
+    // Date and time picker
+    $('.date').datetimepicker({
+        format: 'L'
+    });
+    $('.time').datetimepicker({
+        format: 'LT'
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        margin: 30,
+        dots: true,
+        loop: true,
+        center: true,
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
+    
+})(jQuery);
+
